8629d18ea1caef7cd267f6fc479d90fe4d5c7c88
rutwikreddy <70323495+rutwikreddy@users.noreply.github.com>
2025-11-09 21:01:32 +0530

    Implement chat completion function
    Added function to generate chat completions using an API.

--- chat-completion/chat.py
+++ chat-completion/chat.py
@@ -1,1 +1,26 @@
+import requests
 
+def generate_completion():
+    url = "https://api.euron.one/api/v1/euri/chat/completions"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer abkjbkjfdf"
+    }
+    payload = {
+        "messages": [
+            {
+                "role": "user",
+                "content": "How to do multithreading using GIL in python"
+            }
+        ],
+        "model": "gpt-4.1-nano",
+        "max_tokens": 1000,
+        "temperature": 0.7
+    }
+
+    response = requests.post(url, headers=headers, json=payload)
+    data = response.json()
+    print(data)
+
+if __name__ == "__main__":
+  generate_completion()
